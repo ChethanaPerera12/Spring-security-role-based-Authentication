482d4daaf5f0ac99977dc19a4cf43012cd42efb9
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-06 20:12:38 +0530

    Add request mapping to PublicController

---
+++ src/main/java/edu/icet/spring_auth/controller/AuthController.java
@@ -0,0 +1,20 @@
+package edu.icet.spring_auth.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secure")
+public class AuthController {
+    @GetMapping("/customer")
+    public String sayHelloCustomer(){
+        return "Hello Customer";
+    }
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String sayHelloAdmin(){
+        return "Hello Admin";
+    }
+}
